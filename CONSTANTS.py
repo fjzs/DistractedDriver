9a7d7ff4314d655a78c7a9268cc2350292910dd3
Francisco <zenteno.fco@gmail.com>
2022-07-25 15:46:03 -0400

    lil update

---
+++ CONSTANTS.py
@@ -0,0 +1,11 @@
+CLASSES = ["Safe driving",
+           "Texting right hand",
+           "Talking on the phone right hand",
+           "Texting left hand",
+           "Talking on the phone left hand",
+           "Operating the radio",
+           "Drinking",
+           "Reaching behind",
+           "Hair and/or makeup",
+           "Talking to passenger"
+           ]
